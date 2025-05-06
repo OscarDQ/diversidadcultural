7da188256a678d1ea37c510b40f8685554197e23
OscarDQ <oscar.diaz.qui@gmail.com>
2025-05-06 12:52:20 -0600

    xd

--- contaminacion.js
+++ contaminacion.js
@@ -1,5 +1,18 @@
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', function() {
-        this.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    // Para tarjetas
+    document.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('click', function() {
+            // Cierra todas las tarjetas primero
+            document.querySelectorAll('.card').forEach(c => {
+                c.classList.remove('active');
+                c.querySelector('.card-visible').style.display = 'block';
+                c.querySelector('.card-hidden').style.display = 'none';
+            });
+            
+            // Abre solo esta tarjeta
+            this.classList.add('active');
+            this.querySelector('.card-visible').style.display = 'none';
+            this.querySelector('.card-hidden').style.display = 'block';
+        });
     });
 });
